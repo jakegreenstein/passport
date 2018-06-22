ec1eaf03a7e4afe4cb8986b85ed372e38db08a45
jakegreenstein <jake.a.greenstein@gmail.com>
2018-06-22 14:30:51 -0400

    init

---
+++ config/auth.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1702327573422105', // your App ID
+        'clientSecret'  : 'c54309e40f12011c8973ba6e5b4f68f7', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'instagramAuth' : {
+        'clientID'      : '4e36d4b85e4b46acb846df1341d238c9', // your App ID
+        'clientSecret'  : '44176a97edd94e6b8d29987471a04472', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/instagram/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : '4orCYx3sCTwwi0mwhv2PVDa9z',
+        'consumerSecret'    : 'MCSFMIPm3XItrmQZKPylVweXw5HjKlIC1P8J3pOhfZBL9hNZDX',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '157964441446-sn3e7fsgf51b3641tu8upqlqoi4tkduu.apps.googleusercontent.com',
+        'clientSecret'  : 'dLKq3oL8pw1c1bLm8SYflonk',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
